cfd296910b0fbb9d8fae2c01945561e3a1aac342
fu-mi-man <fumichan0393@gmail.com>
2024-03-01 22:00:39 +0900

    add Response

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -6,15 +6,25 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\BookPostRequest;
 use App\Models\Book;
 use App\Models\Category;
-use Illuminate\Database\Eloquent\Collection;
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response as HttpResponse;
 use Illuminate\View\View;
 
 class BookController extends Controller
 {
-    public function index(): Collection
+    public function index(): HttpResponse
     {
         $books = Book::all();
-        return $books;
+        $books = Book::with('category')
+            ->orderBy('category_id')
+            ->orderBy('title')
+            ->get();
+
+        return response()
+            ->view('admin.book.index', ['books' => $books])
+            ->header('Content-Type', 'text/html')
+            ->header('Content-Encoding', 'UTF-8');
     }
 
     public function show(string $id): Book
@@ -31,13 +41,15 @@ class BookController extends Controller
         ]);
     }
 
-    public function store(BookPostRequest $request): Book
+    public function store(BookPostRequest $request): RedirectResponse
     {
         $book = new Book();
         $book->category_id = $request->category_id;
         $book->title = $request->title;
         $book->price = $request->price;
         $book->save();
-        return $book;
+
+        return redirect(route('book.index'))
+            ->with('message', $book->title . 'を追加しました');
     }
 }
