065ef6e39b58913904ccba34bcd90065b4204253
fu-mi-man <fumichan0393@gmail.com>
2024-03-01 21:35:28 +0900

    add validation

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\BookPostRequest;
 use App\Models\Book;
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class BookController extends Controller
@@ -31,7 +31,7 @@ class BookController extends Controller
         ]);
     }
 
-    public function store(Request $request): Book
+    public function store(BookPostRequest $request): Book
     {
         $book = new Book();
         $book->category_id = $request->category_id;
